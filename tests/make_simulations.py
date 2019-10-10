abda542bbb7c8b2f88e69b68eded8ff69e483ea1
Pierre-Francois Leget <leget@clermont.in2p3.fr>
2019-10-11 00:04:14 +0200

    init comit

---
+++ tests/make_simulations.py
@@ -0,0 +1,12 @@
+import pylab as plt
+import numpy as np
+import galsim
+
+
+## TO DO list: ##
+# load ai maps
+# from ai maps make psfs with cst atmosphere with some bi
+# add real stars position 
+# add gaussian random fields for atmosphere 
+# make realistic snr
+
